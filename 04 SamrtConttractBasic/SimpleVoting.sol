e9ad022e09c66de26128a0498b9a22acba5d73a6
vasilako <vasilako@hotmail.com>
2025-01-09 18:55:59 +0200

    add functions displayPrivateVariable, changeBooleanFlag, displayPrivateIsFlag

--- 04 SamrtConttractBasic/SimpleVoting.sol
+++ 04 SamrtConttractBasic/SimpleVoting.sol
@@ -2,21 +2,36 @@
 
 pragma solidity =0.8.26;
 
-contract SimpleVoting{
+contract SimpleVoting {
+    uint256 private _voteCandidate = 5;
+    bool private _isFlag = false;
 
-    uint256 private  voteCandidate = 5;
-
-    function test (uint256 a) external pure returns (uint256) {
-    
+    function test(uint256 a) external pure returns (uint256) {
         return a + 10;
     }
 
-    function NamedArguments(uint256 a) external pure returns (uint256 b, uint256 c){
-        b= a * 5;
+    function NamedArguments(uint256 a)
+        external
+        pure
+        returns (uint256 b, uint256 c)
+    {
+        b = a * 5;
         c = a * 10;
-    } 
+    }
 
-    function displayPrivateVariable() public view returns (uint256){
-        return voteCandidate;
+    function displayPrivateVariable() public view returns (uint256) {
+        return _voteCandidate;
+    }
+
+    function changeBooleanFlag() public returns (bool) {
+        if (_isFlag == false) {
+            _isFlag = true;
+        } else {
+            _isFlag = false;
+        }
+        return _isFlag;
+    }
+    function displayPrivateIsFlag()public view returns (bool){
+        return _isFlag;
     }
 }
