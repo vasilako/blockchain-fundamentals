3f492af87abd7eb3d0945e9a05b5ebfd48fed440
vasilako <vasilako@hotmail.com>
2025-01-09 18:37:58 +0200

    add function displayPrivateVariable

--- 04 SamrtConttractBasic/SimpleVoting.sol
+++ 04 SamrtConttractBasic/SimpleVoting.sol
@@ -4,6 +4,8 @@ pragma solidity =0.8.26;
 
 contract SimpleVoting{
 
+    uint256 private  voteCandidate = 5;
+
     function test (uint256 a) external pure returns (uint256) {
     
         return a + 10;
@@ -13,4 +15,8 @@ contract SimpleVoting{
         b= a * 5;
         c = a * 10;
     } 
+
+    function displayPrivateVariable() public view returns (uint256){
+        return voteCandidate;
+    }
 }
